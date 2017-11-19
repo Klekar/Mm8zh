5321b246daa6f605b1065179d9e35ae15a2ca659
Karel Klecka <kkleckla@seznam.cz>
2017-11-19 16:13:37 +0100

    up

--- testovac.c
+++ testovac.c
@@ -4,18 +4,19 @@
 
 
 void printStats() {
-    printf("stats\n");
+	printf("stats\n");
 }
 
 
 void *statClock(void *arg)
 {
-    while(1)
-    {
-        sleep(arg[0]);
-        printStats();
-    }
-    return 0;
+	int t = *((itn *) i);
+	while(1)
+	{
+		sleep(t++);
+		printStats();
+	}
+	return 0;
 }
 
 
@@ -23,13 +24,22 @@ void *statClock(void *arg)
  * testovac [-h] [-u] [-t <interval>] [-i <interval>] [-p <port>] [-l <port>] [-s <size>] [-r <value>] <uzel1> <uzel2> <uzel3> ...
  */
 int main(int argc, char** argv) {
-	printf("program zacal");
+
+	int *arg = malloc(sizeof(*arg));
+	if ( arg == NULL ) {
+		fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+		exit(99);
+	}
+	*arg = 0;
 	pthread_t tid;
-	printf("program init");
-	pthread_create(&tid, NULL, &statClock, 1);
-	printf("program vytvoril thread");
+	pthread_create(&tid, NULL, &statClock, arg);
+	for (int i = 0; i < 10; i++) {
+		sleep(3);
+		printf("%d\n", *arg);
+	}
 	sleep(1000);
-    /*pid_t pid;
+
+	/*pid_t pid;
     pid = fork();
     if (pid == 0) {
         printf("child process");
