78c4f1f735cc9724fa6f42b8ca0ae6439b243fd0
Karel Klecka <kkleckla@seznam.cz>
2017-11-19 14:49:54 +0100

    up

--- testovac.c
+++ testovac.c
@@ -23,8 +23,11 @@ void *statClock(void *arg)
  * testovac [-h] [-u] [-t <interval>] [-i <interval>] [-p <port>] [-l <port>] [-s <size>] [-r <value>] <uzel1> <uzel2> <uzel3> ...
  */
 int main(int argc, char** argv) {
+	printf("program zacal");
 	pthread_t tid;
+	printf("program init");
 	pthread_create(&tid, NULL, &statClock, NULL);
+	printf("program vytvoril thread");
 	sleep(1000);
     /*pid_t pid;
     pid = fork();
