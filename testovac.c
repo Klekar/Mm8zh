6694c96af86aa9ffaea30dedaa721f88e0fd4e54
Karel Klecka <kkleckla@seznam.cz>
2017-11-19 14:38:34 +0100

    up

--- testovac.c
+++ testovac.c
@@ -22,12 +22,14 @@ void *statClock(void *arg)
  * testovac [-h] [-u] [-t <interval>] [-i <interval>] [-p <port>] [-l <port>] [-s <size>] [-r <value>] <uzel1> <uzel2> <uzel3> ...
  */
 int main(int argc, char** argv) {
-    pid_t pid;
+	pthread_t tid;
+	pthread_create(&tid, NULL, &statClock, NULL);
+    /*pid_t pid;
     pid = fork();
     if (pid == 0) {
         printf("child process");
     } else {
         sleep(15);
         printf("parent process");
-    }
+    }*/
 }
